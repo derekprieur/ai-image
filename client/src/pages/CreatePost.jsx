0d59a95f3379971cf395dee15b7eca2ed15ee52b
derekprieur <DPrieur123@gmail.com>
2023-01-22 12:31:27 -0500

    updated urls

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,7 +16,7 @@ const CreatePost = () => {
         if (form.prompt && form.photo) {
             setLoading(true)
             try {
-                const response = await fetch(`http://localhost:8080/api/v1/post`, {
+                const response = await fetch(`https://pix-l7lf.onrender.com/api/v1/dalle`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -51,7 +51,7 @@ const CreatePost = () => {
             try {
                 setGeneratingImg(true)
                 console.log('Generating image...')
-                const response = await fetch(`http://localhost:8080/api/v1/dalle`, {
+                const response = await fetch(`https://pix-l7lf.onrender.com/api/v1/dalle`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
